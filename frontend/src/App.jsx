382d94a1d37870506ad65c8ac559102b079cf742
Evan-Codes88 <Evan2401tm@gmail.com>
2025-02-25 18:28:28 +1000

    Feat: Setting up JSX routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Navbar from './components/Navbar';
 
 import HomePage from './pages/HomePage';
@@ -7,16 +6,35 @@ import LoginPage from './pages/LoginPage';
 import SettingsPage from './pages/SettingsPage';
 import ProfilePage from './pages/ProfilePage';
 
-import { Routes, Route } from 'react-router';
+import { Routes, Route, Navigate } from 'react-router';
+import { useAuthStore } from './store/useAuthStore';
+import React, { useEffect } from 'react'
+
+import { Loader } from 'lucide-react';
 
 const App = () => {
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  if(isCheckingAuth && !authUser) 
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
   return (
     <div>
       
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/settings" element={<SettingsPage />} />
