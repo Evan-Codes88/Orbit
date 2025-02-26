1e79474aaefd01f8b41d2858c93d1da63241e44d
Evan-Codes88 <Evan2401tm@gmail.com>
2025-02-26 14:29:20 +1000

    Feat: Completed sign up page with authUser for API communication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import { useAuthStore } from './store/useAuthStore';
 import React, { useEffect } from 'react'
 
 import { Loader } from 'lucide-react';
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
@@ -39,8 +40,9 @@ const App = () => {
         <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
-
       </Routes>
+
+      <Toaster />
 
     </div>
   );
