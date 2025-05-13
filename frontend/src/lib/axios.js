0102dc16855921a044b82fa51a64ddbc29e32747
Evan-Codes88 <Evan2401tm@gmail.com>
2025-05-13 13:10:07 +1000

    Feat: Fixing more code for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "https://orbit-lq87.onrender.com",
     withCredentials: true,
 })
